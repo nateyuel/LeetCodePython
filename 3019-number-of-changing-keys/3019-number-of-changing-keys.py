f5943a4213c6cca3ee0c8bcdbb9428ad0ed741ce
Nate <167537997+nateyuel@users.noreply.github.com>
2026-09-14 17:40:13 +0300

    Time: 3 ms (32.25%), Space: 19.2 MB (52%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        s = s.lower()
+        changes = 0
+
+        for i in range(1, len(s)):
+            if s[i] != s[i-1]:
+                changes += 1
+        
+        return changes

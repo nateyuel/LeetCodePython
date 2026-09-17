af6a57f8053c6474552a9cb10272898a1c6086e4
Nate <167537997+nateyuel@users.noreply.github.com>
2026-09-17 19:56:12 +0300

    Time: 149 ms (57.54%), Space: 39.5 MB (45.81%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -3,18 +3,18 @@ class Solution:
         pos = {0: -1}
         n = len(arr)
         s = 0
-        res = n+1 
+        res = n + 1
         min_len = n
 
         for idx, num in enumerate(arr):
             s += num
             if s - target in pos:
-                j = pos[s-target]
-                length = idx-j
-                res = min(res, length+(n if j==-1 else arr[j]))
+                j = pos[s - target]
+                length = idx - j
+                res = min(res, length + (n if j == -1 else arr[j]))
                 min_len = min(min_len, length)
-            
+
             arr[idx] = min_len
             pos[s] = idx
-        
-        return -1 if res == n+1 else res
+
+        return -1 if res == n + 1 else res
